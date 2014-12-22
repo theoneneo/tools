8d3dbc638b6b9530830078aa3c5914b5609e9555
theoneneo <mobi.liubing@gmail.com>
2014-12-22 11:31:04 +0800

    add

--- bitoatm/src/com/bitocean/atm/struct/RateStruct.java
+++ bitoatm/src/com/bitocean/atm/struct/RateStruct.java
@@ -10,5 +10,12 @@ import java.io.Serializable;
  *
  */
 public class RateStruct implements Serializable{
-	
+	public String bit_type;//虚拟货币币类型
+	public String currency_type;//法币类型
+	public String bit_rate;// 1比特币或火币与法币汇率  423.54 人民币
+	public String currency_rate;// 最小阀值1000兑换多少比特币 0.00020456 比特币
+	public String poundage_buy;// 买入费比率
+	public String poundage_sell;// 卖出手续费比率
+	public String threshold_min;// 最小阀值
+	public String threshold_max;//每次对应币种交易法币最大阀值
 }
